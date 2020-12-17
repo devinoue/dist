ad3118e867428dc0abdc953be90856979884572c
Masaharu Inoue <pasteur1822@gmail.com>
2020-12-17 23:29:54 +0900

    init

---
+++ precache-manifest.37d77afa0b07486c00e641bbbfabff75.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07298f2dd8494c092e64",
+    "url": "/css/app.6dbf23b1.css"
+  },
+  {
+    "revision": "3b9d5ca3ed6c27ddba07",
+    "url": "/css/chunk-vendors.ae9e8f0e.css"
+  },
+  {
+    "revision": "770180cee6f684f69e620ce93381255e",
+    "url": "/fonts/icon.770180ce.ttf"
+  },
+  {
+    "revision": "a128bc109cfda1d50b97b6b060ded811",
+    "url": "/fonts/icon.a128bc10.woff"
+  },
+  {
+    "revision": "a60a36414cba54db4f799417659488be",
+    "url": "/fonts/icon.a60a3641.eot"
+  },
+  {
+    "revision": "485a12cb6c7a339b88df4bc73b8c6fbe",
+    "url": "/img/binance.485a12cb.svg"
+  },
+  {
+    "revision": "450d1a4fb848105d805d2600b91bebb3",
+    "url": "/img/binance_futures.450d1a4f.svg"
+  },
+  {
+    "revision": "4a8ca8e7ec465c4aa3bb531556a5b3fd",
+    "url": "/img/bitfinex.4a8ca8e7.svg"
+  },
+  {
+    "revision": "447dea3806ca725aecdc63c696f37260",
+    "url": "/img/bitmex.447dea38.svg"
+  },
+  {
+    "revision": "31b40fd5c92d4ddc002810dcb33ebac2",
+    "url": "/img/bitstamp.31b40fd5.svg"
+  },
+  {
+    "revision": "353cd67dab9beb87e1ffc31ded241257",
+    "url": "/img/bybit.353cd67d.svg"
+  },
+  {
+    "revision": "4aa101cc541fdc597ad0910baa503c8f",
+    "url": "/img/coinex.4aa101cc.svg"
+  },
+  {
+    "revision": "21f26f1e93db26859722f6d63aed5d28",
+    "url": "/img/deribit.21f26f1e.svg"
+  },
+  {
+    "revision": "2bbe6ec87c01f078731776e637dc71c5",
+    "url": "/img/ftx.2bbe6ec8.svg"
+  },
+  {
+    "revision": "8a95d742e64da26988401306f5e2927c",
+    "url": "/img/gdax.8a95d742.svg"
+  },
+  {
+    "revision": "c82e309c4720a6d17045aa34a183e772",
+    "url": "/img/hitbtc.c82e309c.svg"
+  },
+  {
+    "revision": "44809724927c44dda47c16f3dd91268c",
+    "url": "/img/huobi.44809724.svg"
+  },
+  {
+    "revision": "7dd7f4570ac818215c5bbfc654989d88",
+    "url": "/img/icon.7dd7f457.svg"
+  },
+  {
+    "revision": "c9d9ca03978fea86dd7ae04ac2c8c75b",
+    "url": "/img/kraken.c9d9ca03.svg"
+  },
+  {
+    "revision": "e53c69a628c18ab6e6a72b1d5deaed4a",
+    "url": "/img/liquid.e53c69a6.svg"
+  },
+  {
+    "revision": "dc80a6581017f82b6ff1da8cbc4646ff",
+    "url": "/img/okex.dc80a658.svg"
+  },
+  {
+    "revision": "ba8cf03bbcebd4b15c459c565cd62e4b",
+    "url": "/img/poloniex.ba8cf03b.svg"
+  },
+  {
+    "revision": "06c938363f59ed68f41ffa2cabc1a3c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07298f2dd8494c092e64",
+    "url": "/js/app.ae062b2f.js"
+  },
+  {
+    "revision": "3b9d5ca3ed6c27ddba07",
+    "url": "/js/chunk-vendors.623a006e.js"
+  },
+  {
+    "revision": "b45874ec592abb0e098cc3a28f613835",
+    "url": "/manifest.json"
+  }
+]);
